3aa1689d98c0c1b884ab9f7c92b2376972109420
GAMAMTP <nicandrogama@gmail.com>
2023-01-17 18:10:13 -0600

    delete var not used

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css"; /* 
 import ParticleBackground from "./ParticleBackground";
 import Navbar from "./components/navbar"; */
