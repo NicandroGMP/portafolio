7d4c8a4a26c6644a2f5d11f27ec07a1ba47c96bd
GAMAMTP <nicandrogama@gmail.com>
2023-01-18 21:46:32 -0600

    custom control slider

--- src/@App/core/AppLayout/DefaultLayout/AppBody/index.js
+++ src/@App/core/AppLayout/DefaultLayout/AppBody/index.js
@@ -1,7 +1,73 @@
 import React from "react";
 import { About, Habilities } from "sections";
 import { FullPage, Slide } from "react-full-page/lib";
+import PropTypes from "prop-types";
+class CustomControls extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    getCurrentSlideIndex: PropTypes.func.isRequired,
+    onNext: PropTypes.func.isRequired,
+    onPrev: PropTypes.func.isRequired,
+    scrollToSlide: PropTypes.func.isRequired,
+    slidesCount: PropTypes.number.isRequired,
+    style: PropTypes.object,
+  };
 
+  static defaultProps = {
+    className: "full-page-controls",
+  };
+
+  renderSlidesNumbers(currentSlideIndex) {
+    const { slidesCount, scrollToSlide } = this.props;
+    const slidesNumbers = [];
+    for (let i = 0; i < slidesCount; i++) {
+      const buttonProps = {
+        className: `${
+          i != currentSlideIndex ? "SliderNumber" : "Active SliderNumber"
+        }`,
+        data_push: i != currentSlideIndex ? "" : "Active",
+        disabled: currentSlideIndex === i,
+        key: i,
+        onClick: function onClick() {
+          return scrollToSlide(i);
+        },
+      };
+      slidesNumbers.push(
+        <button type="button" {...buttonProps}>
+          {i + 1}
+        </button>
+      );
+    }
+    return slidesNumbers;
+  }
+
+  render() {
+    const { getCurrentSlideIndex, slidesCount, style, className } = this.props;
+    const currentSlideIndex = getCurrentSlideIndex();
+
+    return (
+      <div className={className} style={style}>
+        <button
+          type="button"
+          disabled={currentSlideIndex === 0}
+          onClick={this.props.onPrev}
+          className="prev"
+        >
+          previous
+        </button>
+        {this.renderSlidesNumbers(currentSlideIndex)}
+        <button
+          className="next"
+          type="button"
+          disabled={currentSlideIndex === slidesCount - 1}
+          onClick={this.props.onNext}
+        >
+          next
+        </button>
+      </div>
+    );
+  }
+}
 const AppBody = () => {
   /*   const ref = useRef(null);
   const handleClick = () => {
@@ -27,7 +93,7 @@ const AppBody = () => {
     <>
       <FullPage
         duration={400}
-        controls
+        controls={CustomControls}
         controlsProps={{ className: "controlsNav" }}
       >
         <Slide>
